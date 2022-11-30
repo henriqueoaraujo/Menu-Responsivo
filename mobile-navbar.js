67b0f2e2141c1307dea76cdf703504f05adaa105
Henrique Araújo <henrique0araujo@hotmail.com>
2022-11-30 18:40:10 -0300

    subindo tentativa de carrocel que deu ruim

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -42,3 +42,22 @@ class MobileNavbar {
     ".nav-list li",
   );
   mobileNavbar.init();
+
+  // Carrocel
+
+  const imgs = document.getElementById("img");
+  const img = document.querySelectorAll("#img img");
+
+  let idx = 0;
+
+  function carrocel(){
+    idx++;
+
+    if(idx > img.length - 1){
+      idx = 0;
+    }
+
+    imgs.style.transform = "translateX(${-idx * 1000}px)";
+  }
+
+  setInterval(carrocel, 1800);
