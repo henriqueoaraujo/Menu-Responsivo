4e3de38b3db1b9d76bdfb12389657934f1c12527
Henrique Araújo <henrique0araujo@hotmail.com>
2022-12-02 00:55:49 -0300

    slider simples do simples only css

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -43,21 +43,3 @@ class MobileNavbar {
   );
   mobileNavbar.init();
 
-  // Carrocel
-
-  const imgs = document.getElementById("img");
-  const img = document.querySelectorAll("#img img");
-
-  let idx = 0;
-
-  function carrocel(){
-    idx++;
-
-    if(idx > img.length - 1){
-      idx = 0;
-    }
-
-    imgs.style.transform = "translateX(${-idx * 1000}px)";
-  }
-
-  setInterval(carrocel, 1800);
